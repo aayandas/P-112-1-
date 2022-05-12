2f87d8ab08620bb66576d46c4f08caa240e1f131
aayandas <94917206+aayandas@users.noreply.github.com>
2022-05-12 20:53:03 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,69 @@
+//https://teachablemachine.withgoogle.com/models/opUIIFJj6/
+
+Webcam.set({
+    width:350,
+    height: 300,
+    image_format : 'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach ( '#camera' );
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log('ml5 version:', ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/opUIIFJj6/model.json',modelLoaded);
+
+function modelLoaded() {
+    console.log('Model loaded!');
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data = "The prediction is " + prediction;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+}
+
+function check()
+{
+    img= document.getElementById('captured_image');
+    classifier.classify(img, gotResults);
+}
+
+function gotResults(error, results) {
+    if(error) {
+        console.error(error);
+    } 
+    else{
+        console.log(results);
+        document.getElementById("result_gesture_name").innerHTML = results[0].label;
+        prediction = results[0].label;
+        speak();
+        if(results[0].label == "Brilliant")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128076;"; 
+            document.getElementById("quote").innerHTML = " This is looking amazing";
+        }
+        if(results[0].label == "Thumbs Up")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128077;";
+            document.getElementById("quote").innerHTML = " All the Very Best";
+        }
+        if(results[0].label == "Victory")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#9996;";
+            document.getElementById("quote").innerHTML = " That was a marvelous victory";
+        }
+    }
+}
+
+
+
